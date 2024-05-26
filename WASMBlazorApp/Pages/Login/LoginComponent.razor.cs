ad3605fea02e1b11a8e244fbe438533c7605f6c3
nikhilmarathe12 <nikhil.marathe12@outlook.com>
2024-05-26 21:01:33 +0530

    Login page changes

--- WASMBlazorApp/Pages/Login/LoginComponent.razor.cs
+++ WASMBlazorApp/Pages/Login/LoginComponent.razor.cs
@@ -4,5 +4,7 @@ namespace WASMBlazorApp.Pages.Login
 {
     public partial class LoginComponent : ComponentBase
     {
+        public string username { get; set; }
+        public string Password { get; set; }
     }
 }
